bfd6cb27dc450e1982ab42d7b2bd2fa4598c608f
silverlutsk <silverlutsk@ukr.net>
2024-02-01 10:07:44 +0200

    new commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const contry = prompt('Enter the name of the country:\n\nUkraine\nGermany\nFrance\nAustria\nBulgaria')
+
+switch(contry){
+    case 'Ukraine':
+    case 'ukraine':
+        alert('Country - Ukraine\nCapital - Kyiv\nOfficial language - Ukrainian\nPolitical system - Parliamentary-presidential republic\nIndependence Day - 24 Aug 1991\nArea - 603 628 km²')
+    break
+
+    case 'Germany':
+    case 'germany':
+        alert('Country - Germany\nCapital - Berlin\nOfficial language - German\nPolitical system - Federal republic\nNational day of Germany - 3 Oct 1990\nArea - 357 592 km²')
+    break
+
+    case 'France':
+    case 'france':
+        alert('Country - France\nCapital - Paris\nOfficial language - French\nPolitical system - Unitary republic of the presidential type\nStorming of the Bastille - 14 July 1789\nArea - 643 801 km²')
+    break
+
+    case 'Austria':
+    case 'austria':
+        alert('Country - Austria\nCapital - Wien\nOfficial language - German\nPolitical system - Federal republic\nNational Day in Austria - 26 oct\nArea - 83 871 km²')
+    break
+
+    case 'Bulgaria':
+    case 'bulgaria':
+        alert('Country - Bulgaria\nCapital - Sofia\nOfficial language - Bulgarian\nPolitical system - Parliamentary republic\nNational Day in Austria - 22 Sep 1908\nArea - 110 994 km²')
+    break
+
+
+
+
+}
